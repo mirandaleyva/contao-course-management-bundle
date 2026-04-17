010e7f7bb5318ec69edcd26e7f814156ca6d4ba0
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-17 14:10:29 +0200

    add language files for backend module labels (de/en)

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['courses'] = ['Kurse', 'Verwaltung der Kurse und Kurstermine'];
---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['courses'] = ['Courses', 'Management of courses and course dates'];
