e0f462901719e2f1d27241adb1b121e021289758
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-04-20 13:29:31 +0200

    feat: add timepicker to course date fields

--- src/Resources/contao/dca/tl_course_date.php
+++ src/Resources/contao/dca/tl_course_date.php
@@ -3,139 +3,141 @@
 use Contao\DC_Table;
 
 $GLOBALS['TL_DCA']['tl_course_date'] = [
-    'config' => [
-        'dataContainer' => DC_Table::class,
-        'ptable' => 'tl_course',
-        'enableVersioning' => true,
-        'sql' => [
-            'keys' => [
-                'id' => 'primary',
-                'pid' => 'index',
-            ],
-        ],
+  'config' => [
+    'dataContainer' => DC_Table::class,
+    'ptable' => 'tl_course',
+    'enableVersioning' => true,
+    'sql' => [
+      'keys' => [
+        'id' => 'primary',
+        'pid' => 'index',
+      ],
     ],
+  ],
 
-    'list' => [
-        'sorting' => [
-            'mode' => 4,
-            'fields' => ['sorting'],
-            'flag' => 1,
-            'panelLayout' => 'sort,filter;search,limit',
-            'headerFields' => ['title'],
-        ],
-        'label' => [
-            'fields' => ['start_date', 'start_time', 'end_date', 'end_time'],
-            'format' => '%s | %s - %s | %s',
-        ],
-        'global_operations' => [
-            'all' => [
-                'href' => 'act=select',
-                'class' => 'header_edit_all',
-                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
-            ],
-        ],
-        'operations' => [
-            'edit' => [
-                'href' => 'act=edit',
-                'icon' => 'edit.svg',
-            ],
-            'copy' => [
-                'href' => 'act=paste&amp;mode=copy',
-                'icon' => 'copy.svg',
-            ],
-            'delete' => [
-                'href' => 'act=delete',
-                'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\'Are you sure you want to delete this entry?\'))return false;Backend.getScrollOffset()"',
-            ],
-            'show' => [
-                'href' => 'act=show',
-                'icon' => 'show.svg',
-            ],
-        ],
+  'list' => [
+    'sorting' => [
+      'mode' => 4,
+      'fields' => ['sorting'],
+      'flag' => 1,
+      'panelLayout' => 'sort,filter;search,limit',
+      'headerFields' => ['title'],
     ],
+    'label' => [
+      'fields' => ['start_date', 'start_time', 'end_date', 'end_time'],
+      'format' => '%s | %s - %s | %s',
+    ],
+    'global_operations' => [
+      'all' => [
+        'href' => 'act=select',
+        'class' => 'header_edit_all',
+        'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
+      ],
+    ],
+    'operations' => [
+      'edit' => [
+        'href' => 'act=edit',
+        'icon' => 'edit.svg',
+      ],
+      'copy' => [
+        'href' => 'act=paste&amp;mode=copy',
+        'icon' => 'copy.svg',
+      ],
+      'delete' => [
+        'href' => 'act=delete',
+        'icon' => 'delete.svg',
+        'attributes' => 'onclick="if(!confirm(\'Are you sure you want to delete this entry?\'))return false;Backend.getScrollOffset()"',
+      ],
+      'show' => [
+        'href' => 'act=show',
+        'icon' => 'show.svg',
+      ],
+    ],
+  ],
 
-    'palettes' => [
-        'default' => '{date_legend},start_date,end_date,start_time,end_time;{location_legend},location;{status_legend},fully_booked,published',
-    ],
+  'palettes' => [
+    'default' => '{date_legend},start_date,end_date,start_time,end_time;{location_legend},location;{status_legend},fully_booked,published',
+  ],
 
-    'fields' => [
-        'id' => [
-            'sql' => "int(10) unsigned NOT NULL auto_increment",
-        ],
-        'pid' => [
-            'foreignKey' => 'tl_course.title',
-            'relation' => [
-                'type' => 'belongsTo',
-                'load' => 'lazy',
-            ],
-            'sql' => "int(10) unsigned NOT NULL default 0",
-        ],
-        'tstamp' => [
-            'sql' => "int(10) unsigned NOT NULL default 0",
-        ],
-        'sorting' => [
-            'sql' => "int(10) unsigned NOT NULL default 0",
-        ],
-        'start_date' => [
-            'inputType' => 'text',
-            'eval' => [
-                'rgxp' => 'date',
-                'datepicker' => true,
-                'tl_class' => 'w50 wizard',
-                'mandatory' => true,
-            ],
-            'sql' => "varchar(10) NOT NULL default ''",
-        ],
-        'end_date' => [
-            'inputType' => 'text',
-            'eval' => [
-                'rgxp' => 'date',
-                'datepicker' => true,
-                'tl_class' => 'w50 wizard',
-                'mandatory' => true,
-            ],
-            'sql' => "varchar(10) NOT NULL default ''",
-        ],
-        'start_time' => [
-            'inputType' => 'text',
-            'eval' => [
-                'rgxp' => 'time',
-                'tl_class' => 'w50',
-                'mandatory' => true,
-            ],
-            'sql' => "varchar(5) NOT NULL default ''",
-        ],
-        'end_time' => [
-            'inputType' => 'text',
-            'eval' => [
-                'rgxp' => 'time',
-                'tl_class' => 'w50',
-                'mandatory' => true,
-            ],
-            'sql' => "varchar(5) NOT NULL default ''",
-        ],
-        'location' => [
-            'inputType' => 'text',
-            'eval' => [
-                'maxlength' => 255,
-                'tl_class' => 'clr long',
-            ],
-            'sql' => "varchar(255) NOT NULL default ''",
-        ],
-        'fully_booked' => [
-            'inputType' => 'checkbox',
-            'eval' => [
-                'tl_class' => 'w50 m12',
-            ],
-            'sql' => "char(1) NOT NULL default ''",
-        ],
-        'published' => [
-            'inputType' => 'checkbox',
-            'eval' => [
-                'tl_class' => 'w50 m12',
-            ],
-            'sql' => "char(1) NOT NULL default ''",
-        ],
+  'fields' => [
+    'id' => [
+      'sql' => "int(10) unsigned NOT NULL auto_increment",
     ],
+    'pid' => [
+      'foreignKey' => 'tl_course.title',
+      'relation' => [
+        'type' => 'belongsTo',
+        'load' => 'lazy',
+      ],
+      'sql' => "int(10) unsigned NOT NULL default 0",
+    ],
+    'tstamp' => [
+      'sql' => "int(10) unsigned NOT NULL default 0",
+    ],
+    'sorting' => [
+      'sql' => "int(10) unsigned NOT NULL default 0",
+    ],
+    'start_date' => [
+      'inputType' => 'text',
+      'eval' => [
+        'rgxp' => 'date',
+        'datepicker' => true,
+        'tl_class' => 'w50 wizard',
+        'mandatory' => true,
+      ],
+      'sql' => "varchar(10) NOT NULL default ''",
+    ],
+    'end_date' => [
+      'inputType' => 'text',
+      'eval' => [
+        'rgxp' => 'date',
+        'datepicker' => true,
+        'tl_class' => 'w50 wizard',
+        'mandatory' => true,
+      ],
+      'sql' => "varchar(10) NOT NULL default ''",
+    ],
+    'start_time' => [
+      'inputType' => 'text',
+      'eval' => [
+        'rgxp' => 'time',
+        'timepicker' => true,
+        'tl_class' => 'w50 wizard',
+        'mandatory' => true,
+      ],
+      'sql' => "varchar(5) NOT NULL default ''",
+    ],
+    'end_time' => [
+      'inputType' => 'text',
+      'eval' => [
+        'rgxp' => 'time',
+        'timepicker' => true,
+        'tl_class' => 'w50 wizard',
+        'mandatory' => true,
+      ],
+      'sql' => "varchar(5) NOT NULL default ''",
+    ],
+    'location' => [
+      'inputType' => 'text',
+      'eval' => [
+        'maxlength' => 255,
+        'tl_class' => 'clr long',
+      ],
+      'sql' => "varchar(255) NOT NULL default ''",
+    ],
+    'fully_booked' => [
+      'inputType' => 'checkbox',
+      'eval' => [
+        'tl_class' => 'w50 m12',
+      ],
+      'sql' => "char(1) NOT NULL default ''",
+    ],
+    'published' => [
+      'inputType' => 'checkbox',
+      'eval' => [
+        'tl_class' => 'w50 m12',
+      ],
+      'sql' => "char(1) NOT NULL default ''",
+    ],
+  ],
 ];
